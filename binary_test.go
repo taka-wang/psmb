4504745b4379c6d30b98e9f9616660f985c3a945
Taka Wang <wang.chia.ming@gmail.com>
2016-06-27 16:45:41 +0800

    Implement int16, uint16, uint32, int32 conversion - add travis and implement float32 conversion

--- binary_test.go
+++ binary_test.go
@@ -1,135 +1,228 @@
-/*
-func main() {
-	var i int64 = 2323
-	buf := Int64ToBytes(i)
-	fmt.Println(buf)
-	fmt.Println(BytesToInt64(buf))
-
-	var arr []uint16 = []uint16{4396, 79, 4660, 22136}
-	buf2 := Uint16ToBytes(arr)
-	buf3 := Uint16To2Bytes(arr)
-	fmt.Println(buf2)
-	fmt.Println(buf3)
-	fmt.Println(hex.EncodeToString(buf2))
-	fmt.Println(hex.EncodeToString(buf3))
-	fmt.Println(Float32frombytes(buf2))
-}
-*/
-
 package psmb
 
-import "testing"
+import (
+	"testing"
 
-func Test_Division_1(t *testing.T) {
-	HexStrToRegs("AB12EF34")
-	t.Log("pass")
-}
+	"github.com/marksalpeter/sugar"
+)
 
-/*
+func Test16bitConvert(t *testing.T) {
+	s := sugar.New(nil)
+	arr := []uint16{4396, 79, 4660, 22136} // 112C004F12345678
 
-func Test_Division_1(t *testing.T) {
-	t.Log("pass")
-}
+	s.Title("Bytes to 16-bit integer array")
 
-func Test_Division_2(t *testing.T) {
-	t.Error("fail")
-}
-
-func Test_Division_3(t *testing.T) {
-	var arr []uint16 = []uint16{4396, 79, 4660, 22136}
-	buf := new(bytes.Buffer)
-	err := Write(buf, MidBigEndian, arr)
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	t.Log(buf.Bytes())
-}
-
-type ByteOrder interface {
-	Uint16([]byte) uint16
-	Uint32([]byte) uint32
-	Uint64([]byte) uint64
-	PutUint16([]byte, uint16)
-	PutUint32([]byte, uint32)
-	PutUint64([]byte, uint64)
-	String() string
-}
-
-// Write my writer
-func Write(w io.Writer, order ByteOrder, data interface{}) error {
-	return binary.Write(w, order.(binary.ByteOrder), data)
-}
-
-
-// BADC
-// DCBA
-func (midBigEndian) Uint32(b []byte) uint32 {
-	//return uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2]) | uint32(b[3])<<24
-
-	return uint32(b[0])<<16 | uint32(b[1])<<24 | uint32(b[2]) | uint32(b[3])<<8
-	//A             //B             //C                 //D
-}
-
-// Mid-Big Endian (BADC)
-
-var MidBigEndian midBigEndian
-
-type midBigEndian struct{}
-
-// Mid-Little Endian (CDAB)
-
-var MidLittle midLittleEndian
-
-type midLittleEndian struct{}
-*/
-/*
-// LittleEndian is the little-endian implementation of ByteOrder.
-var LittleEndian littleEndian
-
-type bigEndian struct{}
-
-// BigEndian is the big-endian implementation of ByteOrder.
-var BigEndian bigEndian
-
-type littleEndian struct{}
-*/
-
-/*
-
-func Float32frombytes(bytes []byte) float32 {
-	bits := binary.LittleEndian.Uint32(bytes)
-	float := math.Float32frombits(bits)
-	return float
-}
-
-func Uint16To2Bytes(data []uint16) []byte {
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.LittleEndian, data)
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
-	}
-	return buf.Bytes()
-}
-
-func Uint16ToBytes(data []uint16) []byte {
-	buf := new(bytes.Buffer)
-	for _, v := range data {
-		err := binary.Write(buf, binary.LittleEndian, v)
-		if err != nil {
-			fmt.Println("binary.Write failed:", err)
+	s.Assert("`byte to uint16` in big endian order", func(log sugar.Log) bool {
+		desire := []uint16{4396, 79, 4660, 22136}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt16s(bytes, 0)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
 		}
-	}
-	return buf.Bytes()
-}
+		return true
+	})
 
-func Int64ToBytes(i int64) []byte {
-	var buf = make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, uint64(i))
-	return buf
-}
+	s.Assert("`byte to uint16` in little endian order", func(log sugar.Log) bool {
+		desire := []uint16{11281, 20224, 13330, 30806}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt16s(bytes, 1)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
 
-func BytesToInt64(buf []byte) int64 {
-	return int64(binary.BigEndian.Uint64(buf))
-}
+	s.Assert("`byte to int16` in big endian order", func(log sugar.Log) bool {
+		desire := []int16{4396, 79, 4660, 22136}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt16s(bytes, 0)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
 
-*/
+	s.Assert("`byte to int16` in little endian order", func(log sugar.Log) bool {
+		desire := []int16{11281, 20224, 13330, 30806}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt16s(bytes, 1)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Title("Bytes to 32-bit integer array")
+
+	s.Assert("`byte to uint32` in (ABCD) Big Endian order", func(log sugar.Log) bool {
+		desire := []uint32{288096335, 305419896}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt32s(bytes, 0)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to uint32` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+		desire := []uint32{1325411345, 2018915346}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt32s(bytes, 1)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to uint32` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+		desire := []uint32{739331840, 873625686}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt32s(bytes, 2)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to uint32` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+		desire := []uint32{5181740, 1450709556}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToUInt32s(bytes, 3)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to int32` in (ABCD) Big Endian  order", func(log sugar.Log) bool {
+		desire := []int32{288096335, 305419896}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt32s(bytes, 0)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to int32` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+		desire := []int32{1325411345, 2018915346}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt32s(bytes, 1)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to int32` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+		desire := []int32{739331840, 873625686}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt32s(bytes, 2)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to int32` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+		desire := []int32{5181740, 1450709556}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToInt32s(bytes, 3)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Title("Bytes to 32-bit float array")
+
+	s.Assert("`byte to float32` in (ABCD) Big Endian order", func(log sugar.Log) bool {
+		arr2 := []uint16{17820, 16863, 17668, 46924} // 459C41DF4504B74C
+		desire := []float32{5000.234, 2123.456}
+		bytes := RegsToBytes(arr2)
+		result, _ := BytesToFloat32s(bytes, 0)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%f, result:%f", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to float32` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+		desire := []float32{2150371580, 1.73782444e34}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToFloat32s(bytes, 1)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%f, result:%f", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to float32` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+		desire := []float32{2.06495931e-12, 1.36410875e-7}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToFloat32s(bytes, 2)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%f, result:%f", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`byte to float32` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+		desire := []float32{7.261164e-39, 68189266400000}
+		bytes := RegsToBytes(arr)
+		result, _ := BytesToFloat32s(bytes, 3)
+		for idx := 0; idx < len(result); idx++ {
+			log("desire:%f, result:%f", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+}
