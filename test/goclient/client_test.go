500ea4f37d7eecfdc6ff87d56dbd0d6367a30416
Taka Wang <wang.chia.ming@gmail.com>
2016-07-08 10:05:32 +0800

    trigger drone

--- test/goclient/client_test.go
+++ test/goclient/client_test.go
@@ -52,7 +52,7 @@ func TestPsmb(t *testing.T) {
 	host, err := net.LookupHost("mbd")
 	if err != nil {
 		fmt.Println("local run")
-		hostName = "172.17.0.4"
+		hostName = "127.0.0.1"
 	} else {
 		fmt.Println("docker run")
 		hostName = host[0] //docker
