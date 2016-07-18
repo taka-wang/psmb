d6cd3fedc3a7228f03fcfc792be772ca6855af45
Taka Wang <wang.chia.ming@gmail.com>
2016-07-18 20:59:07 +0800

    enhance binary test cases

--- binary_test.go
+++ binary_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestBinaryOps(t *testing.T) {
 
-	s := sugar.New(nil)
+	s := sugar.New(t)
 	arr := []uint16{4396, 79, 4660, 22136} // 112C004F12345678
 
 	s.Title("Bytes to 16-bit integer array tests")
 
-	s.Assert("`BytesToUInt16s` in big endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt16s` in big endian order - (1/4)", func(log sugar.Log) bool {
 		desire := []uint16{4396, 79, 4660, 22136}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt16s(bytes, BigEndian)
@@ -27,7 +27,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToUInt16s` in little endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt16s` in little endian order - (2/4)", func(log sugar.Log) bool {
 		desire := []uint16{11281, 20224, 13330, 30806}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt16s(bytes, LittleEndian)
@@ -40,7 +40,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt16s` in big endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt16s` in big endian order - (3/4)", func(log sugar.Log) bool {
 		desire := []int16{4396, 79, 4660, 22136}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt16s(bytes, BigEndian)
@@ -53,7 +53,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt16s` in little endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt16s` in little endian order - (4/4)", func(log sugar.Log) bool {
 		desire := []int16{11281, 20224, 13330, 30806}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt16s(bytes, LittleEndian)
@@ -68,7 +68,7 @@ func TestBinaryOps(t *testing.T) {
 
 	s.Title("Bytes to 32-bit integer array tests")
 
-	s.Assert("`BytesToUInt32s` in (ABCD) Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt32s` in (ABCD) Big Endian order - (1/4)", func(log sugar.Log) bool {
 		desire := []uint32{288096335, 305419896}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt32s(bytes, ABCD)
@@ -81,7 +81,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToUInt32s` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt32s` in (DCBA) Little Endian order - (2/4)", func(log sugar.Log) bool {
 		desire := []uint32{1325411345, 2018915346}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt32s(bytes, DCBA)
@@ -94,7 +94,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToUInt32s` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt32s` in (BADC) Mid-Big Endian order - (3/4)", func(log sugar.Log) bool {
 		desire := []uint32{739331840, 873625686}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt32s(bytes, BADC)
@@ -107,7 +107,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToUInt32s` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToUInt32s` in (CDAB) Mid-Little Endian order - (4/4)", func(log sugar.Log) bool {
 		desire := []uint32{5181740, 1450709556}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToUInt32s(bytes, CDAB)
@@ -120,7 +120,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt32s` in (ABCD) Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt32s` in (ABCD) Big Endian order - (1/4)", func(log sugar.Log) bool {
 		desire := []int32{288096335, 305419896}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt32s(bytes, ABCD)
@@ -133,7 +133,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt32s` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt32s` in (DCBA) Little Endian order - (2/4)", func(log sugar.Log) bool {
 		desire := []int32{1325411345, 2018915346}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt32s(bytes, DCBA)
@@ -146,7 +146,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt32s` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt32s` in (BADC) Mid-Big Endian order - (3/4)", func(log sugar.Log) bool {
 		desire := []int32{739331840, 873625686}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt32s(bytes, BADC)
@@ -159,7 +159,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToInt32s` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToInt32s` in (CDAB) Mid-Little Endian order - (4/4)", func(log sugar.Log) bool {
 		desire := []int32{5181740, 1450709556}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToInt32s(bytes, CDAB)
@@ -174,7 +174,7 @@ func TestBinaryOps(t *testing.T) {
 
 	s.Title("Bytes to 32-bit float array tests")
 
-	s.Assert("`BytesToFloat32s` in (ABCD) Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToFloat32s` in (ABCD) Big Endian order - (1/4)", func(log sugar.Log) bool {
 		arr2 := []uint16{17820, 16863, 17668, 46924} // 459C41DF4504B74C
 		desire := []float32{5000.234, 2123.456}
 		bytes, _ := RegistersToBytes(arr2)
@@ -188,7 +188,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToFloat32s` in (DCBA) Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToFloat32s` in (DCBA) Little Endian order - (2/4)", func(log sugar.Log) bool {
 		desire := []float32{2150371580, 1.73782444e34}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToFloat32s(bytes, DCBA)
@@ -201,7 +201,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToFloat32s` in (BADC) Mid-Big Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToFloat32s` in (BADC) Mid-Big Endian order - (3/4)", func(log sugar.Log) bool {
 		desire := []float32{2.06495931e-12, 1.36410875e-7}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToFloat32s(bytes, BADC)
@@ -214,7 +214,7 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
-	s.Assert("`BytesToFloat32s` in (CDAB) Mid-Little Endian order", func(log sugar.Log) bool {
+	s.Assert("`BytesToFloat32s` in (CDAB) Mid-Little Endian order - (4/4)", func(log sugar.Log) bool {
 		desire := []float32{7.261164e-39, 68189266400000}
 		bytes, _ := RegistersToBytes(arr)
 		result, _ := BytesToFloat32s(bytes, CDAB)
@@ -231,6 +231,19 @@ func TestBinaryOps(t *testing.T) {
 
 	s.Assert("`BitStringToUInt8s` test", func(log sugar.Log) bool {
 		input := "1,0,1,1,0,1"
+		result, _ := BitStringToUInt8s(input)
+		desire := []uint8{1, 0, 1, 1, 0, 1}
+		for idx := 0; idx < len(desire); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`BitStringToUInt8s` test - one more comma", func(log sugar.Log) bool {
+		input := "1,0,1,1,0,1,"
 		result, _ := BitStringToUInt8s(input)
 		desire := []uint8{1, 0, 1, 1, 0, 1}
 		for idx := 0; idx < len(desire); idx++ {
@@ -278,8 +291,60 @@ func TestBinaryOps(t *testing.T) {
 		return true
 	})
 
+	s.Assert("`DecimalStringToRegisters` test - left comma", func(log sugar.Log) bool {
+		input := ",4396,79,4660,22136"
+		result, _ := DecimalStringToRegisters(input)
+		desire := []uint16{4396, 79, 4660, 22136}
+		for idx := 0; idx < len(desire); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`DecimalStringToRegisters` test - right comma", func(log sugar.Log) bool {
+		input := "4396,79,4660,22136,"
+		result, _ := DecimalStringToRegisters(input)
+		desire := []uint16{4396, 79, 4660, 22136}
+		for idx := 0; idx < len(desire); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`DecimalStringToRegisters` test - left, right comma", func(log sugar.Log) bool {
+		input := ",4396,79,4660,22136,"
+		result, _ := DecimalStringToRegisters(input)
+		desire := []uint16{4396, 79, 4660, 22136}
+		for idx := 0; idx < len(desire); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
 	s.Assert("`HexStringToRegisters` test", func(log sugar.Log) bool {
 		input := "112C004F12345678"
+		result, _ := HexStringToRegisters(input)
+		desire := []uint16{4396, 79, 4660, 22136}
+		for idx := 0; idx < len(desire); idx++ {
+			log("desire:%d, result:%d", desire[idx], result[idx])
+			if result[idx] != desire[idx] {
+				return false
+			}
+		}
+		return true
+	})
+
+	s.Assert("`HexStringToRegisters` test - wrong length", func(log sugar.Log) bool {
+		input := "112C004F1234567"
 		result, _ := HexStringToRegisters(input)
 		desire := []uint16{4396, 79, 4660, 22136}
 		for idx := 0; idx < len(desire); idx++ {
