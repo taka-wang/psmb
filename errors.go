50cb737e5a3a035c0c55c09a031fd5225bc60435
Taka Wang <wang.chia.ming@gmail.com>
2016-07-18 22:25:24 +0800

    update error strings

--- errors.go
+++ errors.go
@@ -6,34 +6,34 @@ import "errors"
 
 var (
 	// ErrBitStringToUInt8s is the error of BitStringToUInt8s converter
-	ErrBitStringToUInt8s = errors.New("Converts bit string to uint8 array failed")
+	ErrBitStringToUInt8s = errors.New("Fail to convert bit string to uint8 array")
 
 	// ErrDecimalStringToRegisters is the error of DecimalStringToRegisters converter
-	ErrDecimalStringToRegisters = errors.New("Converts decimal string to uint16/words array in big endian order failed")
+	ErrDecimalStringToRegisters = errors.New("Fail to convert decimal string to uint16/words array in big endian order")
 
 	// ErrHexStringToRegisters is the error of HexStringToRegisters converter
-	ErrHexStringToRegisters = errors.New("Converts hexadecimal string to uint16/words array in big endian order failed")
+	ErrHexStringToRegisters = errors.New("Fail to convert hexadecimal string to uint16/words array in big endian order")
 
 	// ErrRegistersToBytes is the error of RegistersToBytes converter
-	ErrRegistersToBytes = errors.New("Converts registers/uint16 array to byte array in big endian order failed")
+	ErrRegistersToBytes = errors.New("Fail to convert registers/uint16 array to byte array in big endian order")
 
 	// ErrBytesToFloat32s is the error of BytesToFloat32s converter
-	ErrBytesToFloat32s = errors.New("Converts byte array to float32 array in four endian orders failed")
+	ErrBytesToFloat32s = errors.New("Fail to convert byte array to float32 array in four endian orders")
 
 	// ErrNotANumber is the error of LinearScalingRegisters converter
-	ErrNotANumber = errors.New("Scales the registers linearly failed")
+	ErrNotANumber = errors.New("Fail to scale the registers linearly")
 
 	// ErrBytesToInt32s is the error of BytesToInt32s converter
-	ErrBytesToInt32s = errors.New("Converts byte array to UInt32 array in four endian orders failed")
+	ErrBytesToInt32s = errors.New("Fail to convert byte array to UInt32 array in four endian orders")
 
 	// ErrBytesToUInt32s is the error of BytesToUInt32s converter
-	ErrBytesToUInt32s = errors.New("Converts byte array to UInt32 array in four endian orders failed")
+	ErrBytesToUInt32s = errors.New("Fail to convert byte array to UInt32 array in four endian orders")
 
 	// ErrBytesToInt16s is the error of BytesToInt16s converter
-	ErrBytesToInt16s = errors.New("Converts byte array to Int16 array in two endian orders failed")
+	ErrBytesToInt16s = errors.New("Fail to convert byte array to Int16 array in two endian orders")
 
 	// ErrBytesToUInt16s is the error of BytesToUInt16s converter
-	ErrBytesToUInt16s = errors.New("Converts byte array to UInt16 array in two endian orders failed")
+	ErrBytesToUInt16s = errors.New("Fail to convert byte array to UInt16 array in two endian orders")
 )
 
 // Service
@@ -46,17 +46,17 @@ var (
 	ErrRequestNotFound = errors.New("Request not found!")
 
 	// ErrRequestNotSupport is  the error when the request is not support
-	ErrRequestNotSupport = errors.New("Request not support")
+	ErrRequestNotSupport = errors.New("Request not support!")
 
 	// ErrResponseNotSupport is  the error when the response is not support
-	ErrResponseNotSupport = errors.New("Response not support")
+	ErrResponseNotSupport = errors.New("Response not support!")
 
 	// ErrInvalidMessageLength is the error when the length of message is invalid
-	ErrInvalidMessageLength = errors.New("Invalid message length")
+	ErrInvalidMessageLength = errors.New("Invalid message length!")
 
 	// ErrMarshal is the error when marshal to json string failed
-	ErrMarshal = errors.New("Marshal failed")
+	ErrMarshal = errors.New("Fail to marshal!")
 
 	// ErrUnmarshal is the error when unmarshal json string to structure failed
-	ErrUnmarshal = errors.New("Unmarshal failed")
+	ErrUnmarshal = errors.New("Fail to unmarshal!")
 )
