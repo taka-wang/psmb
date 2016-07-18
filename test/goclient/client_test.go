9fa3415041cc437691a9641485e67bc5f7d4b2b7
Taka Wang <wang.chia.ming@gmail.com>
2016-07-18 19:11:38 +0800

    add fail response

--- test/goclient/client_test.go
+++ test/goclient/client_test.go
@@ -676,54 +676,11 @@ func TestOneOffFC6(t *testing.T) {
 		if err := json.Unmarshal([]byte(s2), &r1); err != nil {
 			fmt.Println("json err:", err)
 		}
-		// check response
+		// check fail response
 		if r1.Status != "ok" {
-			return false
+			return true
 		}
-
-		// ---------------- read part
-		readReq := psmb.MbtcpReadReq{
-			From:  "web",
-			Tid:   time.Now().UTC().UnixNano(),
-			IP:    hostName,
-			Port:  portNum1,
-			FC:    3,
-			Slave: 1,
-			Addr:  10,
-			Len:   7,
-			Type:  psmb.RegisterArray,
-		}
-
-		readReqStr, _ := json.Marshal(readReq)
-		cmd = "mbtcp.once.read"
-		go publisher(cmd, string(readReqStr))
-
-		// receive response
-		s1, s2 = subscriber()
-
-		log("req: %s, %s", cmd, string(readReqStr))
-		log("res: %s, %s", s1, s2)
-
-		// parse resonse
-		var data json.RawMessage // raw []byte
-		r2 := psmb.MbtcpReadRes{Data: &data}
-		if err := json.Unmarshal([]byte(s2), &r2); err != nil {
-			fmt.Println("json err:", err)
-		}
-		// check response
-		if r2.Status != "ok" {
-			return false
-		}
-
-		// ---------------- Compare
-		var r3 []uint16
-		if err := json.Unmarshal(data, &r3); err != nil {
-			return false
-		}
-		if r3[0] != 22 {
-			return false
-		}
-		return true
+		return false
 	})
 
 	s.Assert("`FC6` write `HEX` register test: port 502", func(log sugar.Log) bool {
