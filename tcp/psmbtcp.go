bdf1a4d90874a00bb3d6be2a69b5f3e15e476ab3
Taka Wang <wang.chia.ming@gmail.com>
2016-07-29 15:11:38 +0800

    add main

---
+++ tcp/psmbtcp.go
@@ -0,0 +1,20 @@
+package psmbtcp
+
+import "github.com/taka-wang/gocron"
+
+// Dependency injection
+var defaultProactiveService = NewPSMBTCP(
+	NewMbtcpReaderMap(),   // readerMap
+	NewMbtcpWriterMap(),   // writerMap
+	gocron.NewScheduler(), // scheduler
+)
+
+// Start start default proactive service
+func Start() {
+	defaultProactiveService.Start()
+}
+
+// Stop stop default proactive service
+func Stop() {
+	defaultProactiveService.Stop()
+}
