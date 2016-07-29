29fc830dc435e84572811030dddeba6c692b8b17
Taka Wang <wang.chia.ming@gmail.com>
2016-07-29 11:12:45 +0800

    sanitize

--- psmb.go
+++ psmb.go
@@ -2,19 +2,19 @@ package psmb
 
 import "github.com/taka-wang/gocron"
 
-var readerMap = NewMbtcpReaderMap()
-var writerMap = NewMbtcpWriterMap()
-var defaultScheduler = gocron.NewScheduler()
-
 // Dependency injection
-var defaultProactiveService = NewPSMBTCP(readerMap, writerMap, defaultScheduler)
+var defaultProactiveService = NewPSMBTCP(
+	NewMbtcpReaderMap(),   // readerMap
+	NewMbtcpWriterMap(),   // writerMap
+	gocron.NewScheduler(), // scheduler
+)
 
-// Start start bridge
+// Start start default proactive service
 func Start() {
 	defaultProactiveService.Start()
 }
 
-// Stop stop bridge
+// Stop stop default proactive service
 func Stop() {
 	defaultProactiveService.Stop()
 }
