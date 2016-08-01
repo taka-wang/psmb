22adcba11a02a57d7ecef905b22cbb1a26c7a898
Taka Wang <wang.chia.ming@gmail.com>
2016-08-01 12:12:39 +0800

    fix typo

--- tcp-srv/main.go
+++ tcp-srv/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	writerDataStore, _ := psmb.CreateWriterTaskDataStore(&map[string]string{
+	writerDataStore, _ := mbtcp.CreateWriterTaskDataStore(&map[string]string{
 		"DATASTORE": "memory",
 	})
 
