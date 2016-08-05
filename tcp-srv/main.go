d9764b6a493b6f044e9a4ba41d7aa4b944e19654
Taka Wang <wang.chia.ming@gmail.com>
2016-08-05 16:40:11 +0800

    enable mongo history

--- tcp-srv/main.go
+++ tcp-srv/main.go
@@ -4,6 +4,7 @@ import (
 	cron "github.com/taka-wang/psmb/cron"
 	mreader "github.com/taka-wang/psmb/mem-reader"
 	mwriter "github.com/taka-wang/psmb/mem-writer"
+	mgohistory "github.com/taka-wang/psmb/mgo-history"
 	history "github.com/taka-wang/psmb/redis-history"
 	rwriter "github.com/taka-wang/psmb/redis-writer"
 	mbtcp "github.com/taka-wang/psmb/tcp"
@@ -15,16 +16,17 @@ func init() {
 	mbtcp.Register("MemWriter", mwriter.NewDataStore)
 	mbtcp.Register("RedisWriter", rwriter.NewDataStore)
 	mbtcp.Register("History", history.NewDataStore)
+	mbtcp.Register("MgoHistory", mgohistory.NewDataStore)
 	mbtcp.Register("Cron", cron.NewScheduler)
 }
 
 func main() {
 	// dependency injection & factory pattern
 	if srv, _ := mbtcp.NewService(
-		"MemReader", // Reader Data Store
-		"MemWriter", // Writer Data Store
-		"History",   // History Data Store
-		"Cron",      // Scheduler
+		"MemReader",  // Reader Data Store
+		"MemWriter",  // Writer Data Store
+		"MgoHistory", // History Data Store
+		"Cron",       // Scheduler
 	); srv != nil {
 		srv.Start()
 	}
