87f6bb8c27810c572c9a0ae1d7378088808040ef
Taka Wang <wang.chia.ming@gmail.com>
2016-08-06 11:24:06 +0800

    update run script

--- redis-history/test.sh
+++ redis-history/test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# test command
+# color code ---------------
+COLOR_REST='\e[0m'
+COLOR_GREEN='\e[1;32m';
+COLOR_RED='\e[1;31m';
+
+
+# test command -------------
 go test -v
 
 if [ $? -eq 0 ]
 then
-  echo "<<<Test PASS>>>"
-  touch /var/tmp/success
+  #echo "<<<Test PASS>>>"
+  echo -e "${COLOR_RED}<<<Test PASS>>>${COLOR_REST}"
+  touch /var/tmp/success # symbol
   exit 0
 else
-  echo "<<<TEST FAIL>>>" >&2
+  #echo "<<<TEST FAIL>>>" >&2
+  echo -e "${COLOR_GREEN}<<<Test PASS>>>${COLOR_REST}"
   exit 1
 fi
