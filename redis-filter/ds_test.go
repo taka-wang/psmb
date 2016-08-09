c7b26472da522c7814c04786ea51e6785822c1df
Taka Wang <wang.chia.ming@gmail.com>
2016-08-09 12:16:44 +0800

    implement redis-filter test cases:

---
+++ redis-filter/ds_test.go
@@ -0,0 +1,114 @@
+package filter_test
+
+import (
+	"testing"
+
+	"github.com/taka-wang/psmb"
+	rf "github.com/taka-wang/psmb/redis-filter"
+	psmbtcp "github.com/taka-wang/psmb/tcp"
+	"github.com/takawang/sugar"
+)
+
+func init() {
+	psmbtcp.Register("Filter", rf.NewDataStore)
+}
+
+func TestFilter(t *testing.T) {
+	s := sugar.New(t)
+
+	s.Assert("``add` task to map", func(log sugar.Log) bool {
+		filterMap, err := psmbtcp.FilterDataStoreCreator("Filter")
+		log(err)
+		if err != nil {
+			return false
+		}
+
+		a := psmb.MbtcpFilterStatus{
+			Tid:     1234,
+			From:    "test",
+			Poll:    "A",
+			Name:    "B",
+			Enabled: true,
+		}
+		b := psmb.MbtcpFilterStatus{
+			Tid:     123456,
+			From:    "test",
+			Poll:    "A1",
+			Name:    "B1",
+			Enabled: true,
+		}
+
+		// ADD
+		log("Add A item")
+		filterMap.Add(a.Poll, a)
+		log("Add B item")
+		filterMap.Add(b.Poll, b)
+
+		// GET
+		log("GET A item")
+		if r, b := filterMap.Get(a.Poll); b != false {
+			log(r)
+		} else {
+			return false
+		}
+
+		// TOGGLE A
+		log("Toggle A item")
+		if err := filterMap.UpdateToggle(a.Poll, false); err != nil {
+			return false
+		}
+		// GET
+		log("GET A item")
+		if r, b := filterMap.Get(a.Poll); b != false {
+			log(r)
+		} else {
+			return false
+		}
+
+		// GET ALL
+		log("Get all items")
+		if r := filterMap.GetAll(a.Poll); r != nil {
+			log(r)
+		} else {
+			return false
+		}
+
+		// Toggle all
+		log("Toggle all items")
+		filterMap.UpdateAllToggles(false)
+
+		// GET ALL
+		log("Get all items")
+		if r := filterMap.GetAll(a.Poll); r != nil {
+			log(r)
+		} else {
+			return false
+		}
+
+		// DELETE
+		log("Remove A item")
+		filterMap.Delete(a.Poll)
+
+		// GET ALL
+		log("Get all items")
+		if r := filterMap.GetAll(a.Poll); r != nil {
+			log(r)
+		} else {
+			return false
+		}
+
+		// DELETe ALL
+		log("Delete all items")
+		filterMap.DeleteAll()
+
+		// GET ALL
+		log("Get all items")
+		if r := filterMap.GetAll(a.Poll); r == nil {
+			log("empty")
+			return true
+		}
+
+		return false
+
+	})
+}
