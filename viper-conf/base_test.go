b8c3f35b668bf518b9071501def703e2b9868623
Taka Wang <wang.chia.ming@gmail.com>
2016-08-10 22:19:42 +0800

    re-export fields

--- viper-conf/base_test.go
+++ viper-conf/base_test.go
@@ -19,10 +19,6 @@ func TestLogger(t *testing.T) {
 		conf.Log.Debug("hello world")
 		conf.Log.WithError(ErrFilterNotFound).Error("World hello")
 
-		conf.Log.Fields()
-		log.Fields{"a": b}
-
-		conf.Log.Fields
 		conf.Log.WithFields(conf.Fields{
 			"err":       ErrFilterNotFound,
 			"file name": "Hello",
