246f0a2929fa1c1f68635074b0a822e77352fdb9
Taka Wang <wang.chia.ming@gmail.com>
2016-08-10 15:29:40 +0800

    add unit test to mini-conf

---
+++ mini-conf/base_test.go
@@ -0,0 +1,23 @@
+package conf_test
+
+import (
+	"errors"
+	"testing"
+
+	conf "github.com/taka-wang/psmb/mini-conf"
+	"github.com/takawang/sugar"
+)
+
+var (
+	ErrFilterNotFound = errors.New("Filter not found")
+)
+
+func TestLogger(t *testing.T) {
+	s := sugar.New(t)
+
+	s.Assert("Init logger", func(log sugar.Log) bool {
+		conf.Log.Debug("hello world")
+		conf.Log.WithError(ErrFilterNotFound).Error("World hello")
+		return true
+	})
+}
