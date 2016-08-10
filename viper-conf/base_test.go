e79137b6b48902017915677abfdc7f776e93f383
Taka Wang <wang.chia.ming@gmail.com>
2016-08-10 14:53:40 +0800

    done conf unit test

--- viper-conf/base_test.go
+++ viper-conf/base_test.go
@@ -18,7 +18,7 @@ func TestLogger(t *testing.T) {
 	s.Assert("Init logger", func(log sugar.Log) bool {
 		conf.Log.Debug("hello world")
 		var ErrFilterNotFound = errors.New("Filter not found")
-		log.WithError(ErrFilterNotFound).Debug("Hello world")
+		conf.Log.WithError(ErrFilterNotFound).Debug("Hello world")
 		return true
 	})
 }
