9ee685944ae8b5c0ce5a2b07c3bafa4c0a452a08
Taka Wang <wang.chia.ming@gmail.com>
2016-08-13 00:34:10 +0800

    update test script

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,4 @@ RUN glide install
 RUN apt-get update && apt-get install curl -y
 
 # run test
-RUN go test -v
-RUN bash <(curl -s https://codecov.io/bash) -t 558aa53d-c58d-4df4-a1c1-a22a6e6d8572
+RUN ./test.sh
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# color code ---------------
+COLOR_REST='\e[0m'
+COLOR_GREEN='\e[1;32m';
+COLOR_RED='\e[1;31m';
+
+
+# test command -------------
+go test -v
+bash <(curl -s https://codecov.io/bash) -t 558aa53d-c58d-4df4-a1c1-a22a6e6d8572
+
+if [ $? -eq 0 ]
+then
+  #echo "<<<Test PASS>>>"
+  echo -e "${COLOR_RED}<<<Test PASS>>>${COLOR_REST}"
+  touch /var/tmp/success # symbol
+  exit 0
+else
+  #echo "<<<TEST FAIL>>>" >&2
+  echo -e "${COLOR_GREEN}<<<Test PASS>>>${COLOR_REST}"
+  exit 1
+fi
