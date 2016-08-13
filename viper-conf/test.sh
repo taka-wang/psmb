c6d93d9185c6749893747868a9a70da59353eab8
Taka Wang <wang.chia.ming@gmail.com>
2016-08-14 01:25:16 +0800

    include viper-conf to codecov

---
+++ viper-conf/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# color code ---------------
+COLOR_REST='\e[0m'
+COLOR_GREEN='\e[1;32m';
+COLOR_RED='\e[1;31m';
+
+
+# test command -------------
+#go test -v
+go test -v -coverprofile=coverage.txt -covermode=count
+cat coverage.txt >> /shared/coverage.txt && ls
+
+if [ $? -eq 0 ]
+then
+  #echo "<<<Test PASS>>>"
+  echo -e "${COLOR_RED}<<<Test PASS>>>${COLOR_REST}"
+  touch /var/tmp/success # symbol
+  exit 0
+else
+  #echo "<<<TEST FAIL>>>" >&2
+  echo -e "${COLOR_GREEN}<<<Test PASS>>>${COLOR_REST}"
+  exit 1
+fi
